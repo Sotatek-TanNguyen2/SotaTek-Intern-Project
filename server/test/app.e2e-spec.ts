c540055f6e11604f2f5d961e1961706780bee200
Tan Nguyen Ngoc <tan.nguyen2@sotatek.com>
2022-12-05 22:10:22 +0700

    test add feature

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,24 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('App (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      })
+    );
     await app.init();
+    await app.listen(4000);
+    pactum.request.setBaseUrl('http://localhost:4000');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('POST /add', () => {
+    it('should return 3 if firstNumber = 1, secondNumber = 2', () => {
+      return pactum
+        .spec()
+        .post('/add')
+        .withBody({ firstNumber: 1, secondNumber: 2 })
+        .expectStatus(200)
+        .expectBody({ result: 3 });
+    });
+    it('should return 32.57 if firstNumber = 12.45, secondNumber = 20.12', () => {
+      return pactum
+        .spec()
+        .post('/add')
+        .withBody({ firstNumber: 12.45, secondNumber: 20.12 })
+        .expectStatus(200)
+        .expectBody({ result: 32.57 });
+    });
+    it('should throw error if firstNumber is string', () => {
+      return pactum
+        .spec()
+        .post('/add')
+        .withBody({ firstNumber: '123456789123', secondNumber: 2 })
+        .expectStatus(400);
+    });
+    it('should throw error if secondNumber is string', () => {
+      return pactum
+        .spec()
+        .post('/add')
+        .withBody({ firstNumber: 123456789123, secondNumber: '2121' })
+        .expectStatus(400);
+    });
+    it('should throw error if firstNumber or secondNumber is BigInt', () => {
+      return pactum
+        .spec()
+        .post('/add')
+        .withBody({ firstNumber: 1111111111111111111111111, secondNumber: 2 })
+        .expectStatus(400);
+    });
   });
 });
