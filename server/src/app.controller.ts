26b0249a8afe1e86644dd37af81db7b82a912dbc
Tan Nguyen Ngoc <tan.nguyen2@sotatek.com>
2022-12-05 22:06:21 +0700

    feature add

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Input } from './dto/input.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @HttpCode(HttpStatus.OK)
+  @Post('add')
+  add(@Body() data: Input): { result: number } {
+    return this.appService.add(data);
   }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { Input } from './dto/input.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  add(data: Input): { result: number } {
+    if (data.firstNumber > Number.MAX_SAFE_INTEGER) {
+      throw new BadRequestException('firstNumber is too big');
+    }
+    if (data.secondNumber > Number.MAX_SAFE_INTEGER) {
+      throw new BadRequestException('secondNumber is too big');
+    }
+    return { result: data.firstNumber + data.secondNumber };
   }
 }
